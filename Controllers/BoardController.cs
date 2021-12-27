00c2c02a61d1184966f9b92a8736d29bce2aa741
Gunter <ramses203@naver.com>
2021-12-27 22:36:56 +0900

    Add files via upload

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    public class BoardController : Controller
+    {
+        public ActionResult List(int? Id) 
+        {
+            if (Id == null)
+                return Content("ErrorMessage #1");
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "My First Application!";
+        }
+    }
+}
