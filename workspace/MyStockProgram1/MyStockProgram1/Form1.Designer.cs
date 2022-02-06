7390f38772deedddbc215abcc9d98e738bc006d9
Gunter <ramses203@naver.com>
2022-02-06 22:22:07 +0900

    Add files via upload

---
+++ workspace/MyStockProgram1/MyStockProgram1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MyStockProgram1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.codeListButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(19, 8);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(75, 38);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(19, 57);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "로그인 창";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // codeListButton
+            // 
+            this.codeListButton.Location = new System.Drawing.Point(18, 90);
+            this.codeListButton.Name = "codeListButton";
+            this.codeListButton.Size = new System.Drawing.Size(75, 23);
+            this.codeListButton.TabIndex = 2;
+            this.codeListButton.Text = "종목리스트";
+            this.codeListButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.codeListButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button codeListButton;
+    }
+}
+
---
+++ workspace/MyStockProgram1/MyStockProgram1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyStockProgram1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            loginButton.Click += LoginButton_Click;
+            codeListButton.Click += CodeListButtonClick;
+            axKHOpenAPI1.OnEventConnect += API_OnEventConnect;
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("버튼이 눌렸습니다.");
+            // 로그인창 띄우기
+            axKHOpenAPI1.CommConnect(); //axKHOpenAPI1 이 참조변수를 이용해서 키움증권에서 데이터를 받아올수 있게된다.
+        }
+        
+        private void CodeListButtonClick(object sender, EventArgs e)
+        {
+            // 인자값으로 null을 전달하면 전체 종목조회가능
+            string codeList = axKHOpenAPI1.GetCodeListByMarket(null);
+            Console.WriteLine(codeList);
+            string[] codeArray = codeList.Split(';');
+
+            foreach(string code in codeArray)
+            {
+                string itemName = axKHOpenAPI1.GetMasterCodeName(code);
+                Console.WriteLine(code + ' ' + itemName);
+            }
+
+
+            // 총 코드갯수 나타내는 부분
+            int WordCount = 1;
+            foreach(char i in codeList)
+            {
+                if (i == ';') { WordCount++; } 
+            }
+            Console.WriteLine("총 종목코드는 "+ WordCount+" 개 입니다.");
+
+
+
+
+        }
+
+        private void API_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            Console.WriteLine("사용자가 로그인 시도를 했습니다.");
+            if (e.nErrCode == 0)
+            {
+                Console.WriteLine("로그인 성공");
+            }else
+            {
+                Console.WriteLine("로그인 실패");
+            }
+        }
+
+
+    }
+}
